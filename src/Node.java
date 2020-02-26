d64de998f7ec1cc475856c0d1e8713e12b4ffd9c
RyanVillenaUCR <rvill028@ucr.edu>
2020-02-26 05:21:02 -0800

    Added getCost(), plus the other essential 3 java fxns

--- src/Node.java
+++ src/Node.java
@@ -12,6 +12,40 @@ public class Node implements Comparable<Node> {
 		depth = (parent == null ? 0 : parent.depth + 1);
 	}
 	
+	@Override
+	public String toString() {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("-------------\n");
+		sb.append((parent == null ? "Root node\n" : "Not a root node\n"));
+		sb.append("Depth: " + depth + "\n");
+		sb.append("Heuristic: " + heuristic.getClass().getSimpleName() + "\n");
+		sb.append(data);
+		sb.append("-------------\n");
+		
+		return sb.toString();
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		
+		if (!(o instanceof Node)) return false;
+		
+		Node other = (Node) o;
+		
+		return this.data.equals(other.data)
+				&& this.parent == other.parent
+				&& this.depth == other.depth
+				&& this.heuristic.getClass() == other.heuristic.getClass();
+	}
+
+	@Override
+	public int hashCode() {
+		
+		return data.hashCode() + this.heuristic.getClass().toString().hashCode()
+				- (depth * 31) - (parent == null? 31 : 0);
+	}
+	
 	Set<Node> generateChildren() {
 		
 		Set<PuzzleState.Command> cmds = data.possibleCommands();
@@ -39,8 +73,12 @@ public class Node implements Comparable<Node> {
 	@Override
 	public int compareTo(Node other) {
 		
-		return (int) Math.signum((this.depth + this.heuristic.getHeuristic())
-			- (other.depth + other.heuristic.getHeuristic()));
+		return (int) Math.signum(this.getCost() - other.getCost());
+	}
+	
+	public int getCost() {
+		
+		return depth + heuristic.getHeuristic();
 	}
 	
 	public Stack<Node> stackTrace() {
