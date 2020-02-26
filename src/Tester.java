6388d89b733f9808e30e892b20b8f7697bdd92e6
RyanVillenaUCR <rvill028@ucr.edu>
2020-02-26 02:36:08 -0800

    Cleaned up tests, wrote test for Misp Tile

--- src/Tester.java
+++ src/Tester.java
@@ -46,39 +46,39 @@ public class Tester {
 	
 	public static void testManhattanDist() {
 		
-		// To enable these tests,
+		// To enable all tests,
 		// turn ManhattanDistHeuristic.getManhattanSum() public, or
 		// turn ManhattanDistHeuristic.getElementManhattanDist() public
 		
 		// Expected Manhattan distance: 10 w/o empty tile, 12 with
-//		PuzzleState p1 = new PuzzleState(new int[][] {
-//			{ 8, 1, 3 },
-//			{ 4, 0, 2 },
-//			{ 7, 6, 5 }
-//		});
-//		ManhattanDistHeuristic mdh1 = new ManhattanDistHeuristic(p1, false);
-//	
-//		int testElement = 0;
+		PuzzleState p1 = new PuzzleState(new int[][] {
+			{ 8, 1, 3 },
+			{ 4, 0, 2 },
+			{ 7, 6, 5 }
+		});
+		IHeuristic mdh1 = new ManhattanDistHeuristic(p1, false);
+	
+		int testElement = 0;
 		
 //		System.out.println("Testing Manhattan distance for grid\n" + p1
 //				+ "for element " + Integer.toString(testElement) + ": "
 //				+ "MhtnDist(" + Integer.toString(testElement) + "): "
 //				+ mdh1.getElementManhattanDist(testElement));
 		
-//		System.out.println("Testing Manhattan distance for grid\n" + p1);
-//		System.out.println("Manhattan distance: " + mdh1.getManhattanSum());
+		System.out.println("Testing Manhattan distance for grid\n" + p1);
+		System.out.println("Manhattan distance: " + mdh1.getHeuristic());
 		
 		
 		
-//		PuzzleState p2 = new PuzzleState(new int[][] {
-//			{ 1, 2, 3 },
-//			{ 4, 5, 6 },
-//			{ 7, 8, 0 }
-//		});
-//		ManhattanDistHeuristic mdh2 = new ManhattanDistHeuristic(p2);
-//		
-//		System.out.println("Testing Manhattan distance for grid\n" + p2);
-//		System.out.println("Manhattan distance: " + mdh2.getManhattanSum());
+		PuzzleState p2 = new PuzzleState(new int[][] {
+			{ 1, 2, 3 },
+			{ 4, 5, 6 },
+			{ 7, 8, 0 }
+		});
+		IHeuristic mdh2 = new ManhattanDistHeuristic(p2, false);
+		
+		System.out.println("Testing Manhattan distance for grid\n" + p2);
+		System.out.println("Manhattan distance: " + mdh2.getHeuristic());
 		
 //		int testElement = 0;
 ////		
@@ -86,5 +86,30 @@ public class Tester {
 //				+ "for element " + Integer.toString(testElement) + ": "
 //				+ "MhtnDist(" + Integer.toString(testElement) + "): "
 //				+ mdh2.getElementManhattanDist(testElement));
+	}
+
+	public static void testMisplacedTile() {
+		
+		PuzzleState p1 = new PuzzleState(new int[][] {
+			{ 8, 1, 3 },
+			{ 4, 0, 2 },
+			{ 7, 6, 5 }
+		});
+		IHeuristic mpth1 = new MisplacedTileHeuristic(p1, false);
+		
+		System.out.println("Testing Misplaced tile for grid\n" + p1);
+		System.out.println("Misplaced tile: " + mpth1.getHeuristic());
+		
+		
+		
+		PuzzleState p2 = new PuzzleState(new int[][] {
+			{ 1, 2, 3 },
+			{ 4, 5, 6 },
+			{ 7, 8, 0 }
+		});
+		IHeuristic mpth2 = new MisplacedTileHeuristic(p2, false);
+		
+		System.out.println("Testing Misplaced tile for grid\n" + p2);
+		System.out.println("Misplaced tile: " + mpth2.getHeuristic());
 	}
 }
