379bfa3b3fb4038c1a336ea122f56f69661aac48
RyanVillenaUCR <rvill028@ucr.edu>
2020-02-26 05:19:45 -0800

    Verified Nodes work with priority queues

--- src/Tester.java
+++ src/Tester.java
@@ -1,3 +1,5 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Tester {
 
@@ -58,7 +60,7 @@ public class Tester {
 		});
 		IHeuristic mdh1 = new ManhattanDistHeuristic(p1, false);
 	
-		int testElement = 0;
+//		int testElement = 0;
 		
 //		System.out.println("Testing Manhattan distance for grid\n" + p1
 //				+ "for element " + Integer.toString(testElement) + ": "
@@ -118,5 +120,65 @@ public class Tester {
 		new PuzzleState();
 	}
 
+	public static void testNode() {
+		
+		PuzzleState unsolved = new PuzzleState(
+				new int[][] {
+					{ 8, 1, 3 },
+					{ 4, 0, 2 },
+					{ 7, 6, 5 }});
+		
+		Node unsolvedNode = new Node(unsolved, null, new ManhattanDistHeuristic(unsolved, false));
+		
+		System.out.println(unsolvedNode);
+		System.out.println("Cost: " + unsolvedNode.getCost());
+	}
+	
+	public static void testPriorityQueue() {
+		
+		PriorityQueue<Node> pq = new PriorityQueue<Node>();
+		
+		PuzzleState solved = new PuzzleState(
+				new int[][] {
+					{ 1, 2, 3 },
+					{ 4, 5, 6 },
+					{ 7, 8, 0 }});
+		
+		PuzzleState unsolved = new PuzzleState(
+				new int[][] {
+					{ 1, 2, 3 },
+					{ 4, 8, 0 },
+					{ 7, 6, 5 }});
+		
+		Node solvedNode   = new Node(solved,   null, new ManhattanDistHeuristic(solved, false));
+		Node unsolvedNode = new Node(unsolved, null, new ManhattanDistHeuristic(unsolved, false));
+		
+		System.out.println("solvedNode.cost():   " + solvedNode.getCost());
+		System.out.println("unsolvedNode.cost(): " + unsolvedNode.getCost());
+		
+//		pq.add(solvedNode);	// test out of order
+		pq.add(unsolvedNode);
+		pq.add(solvedNode);
+		
+		System.out.println("Popping...\n" + pq.remove());
+		System.out.println("Popping...\n" + pq.remove());
+	}
+	
+	public static void doTests() {
+		
+//		testGrid();
+		
+//		testManhattanDist();
+		
+//		testGridIterator();
+		
+//		testMisplacedTile();
+		
+//		testGetPuzzleFromUser();
+		
+//		testNode();
+		
+		testPriorityQueue();
+	}
 
 }
