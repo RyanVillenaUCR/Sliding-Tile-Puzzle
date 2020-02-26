e3e1042af8988548ef09afe532a4b25271e86ebb
RyanVillenaUCR <rvill028@ucr.edu>
2020-02-26 02:35:52 -0800

    Implemented and tested Misplaced Tile

--- src/MisplacedTileHeuristic.java
+++ src/MisplacedTileHeuristic.java
@@ -1,10 +1,53 @@
 
 public class MisplacedTileHeuristic implements IHeuristic {
 
+	public MisplacedTileHeuristic(PuzzleState p, boolean accountForEmptyTile) {
+		
+		this.p = p;
+		this.accountForEmptyTile = accountForEmptyTile;
+	}
+	
 	@Override
 	public int getHeuristic() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return getMisplacedTileHeuristic();
 	}
+	
+	private int getMisplacedTileHeuristic() {
+		
+		int sum = 0;
+		
+		for (Integer i : p)
+	
+			if (i != 0 || accountForEmptyTile) {
+				sum += getElementMisplacedTileHeuristic(i);
+			}
+
+		return sum;
+	}
+	
+	private int getElementMisplacedTileHeuristic(Integer element) {
+		
+		// Code copied from ManhattanDistHeuristic
+		// (I wrote that too, no plagiarism here dw)
+		int expectedX;
+		int expectedY;
+		
+		if (element == 0)
+			expectedX = expectedY = p.gridWidth() - 1;
+			
+		else {
+			
+			expectedX = (element - 1) % p.gridWidth();
+			expectedY = (element - 1) / p.gridWidth();
+		}
+		
+		// Doesn't check for duplicate tiles in p,
+		// this should be handled by p
+		return p.get(expectedX, expectedY) == element ? 0 : 1;
+	}
+	
+	private PuzzleState p;
+	boolean accountForEmptyTile;
 
 }
